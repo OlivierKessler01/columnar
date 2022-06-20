44c65634f8f4d7021ff1d0760b1744fd80cf1486
Olivier kessler <olivier.kessler@protonmail.com>
2022-06-20 16:46:07 +0200

    Introduce the first layer of daemonization of the server.

--- main.c
+++ main.c
@@ -1,9 +1,38 @@
 #include "filesystem/configuration.h"
+#include "process/daemonize.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    load_configuration();
-    return 0;
+    struct config;
+    FILE * user_log_file;
+
+    config = load_configuration();
+    build_daemon();
+    user_log_file = fopen(config.log_file_path, "w");
+    /* TODO : write log to the file*/
+    
+    if (user_log_file == NULL)
+    {
+        perror("The value provided for log_file_path isn't a writable file.\n");
+        fclose(user_log_file);
+        exit(EXIT_FAILURE);
+    }
+
+    fclose(user_log_file);
+
+    while (1)
+    {
+        //TODO: Insert daemon code here.
+        syslog (LOG_NOTICE, "First daemon started.");
+        sleep (20);
+        break;
+    }
+
+    /* Log to the syslog */
+    syslog (LOG_NOTICE, "First daemon terminated.");
+    closelog();
+
+    return EXIT_SUCCESS;
 }
---
+++ process/daemonize.c
@@ -0,0 +1,71 @@
+/*
+ * daemonize.c
+ * This script daemonizes a process.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <syslog.h>
+#include "daemonize.h"
+
+static void build_daemon()
+{
+    pid_t pid;
+
+    /* Fork off the parent process */
+    pid = fork();
+
+    /* An error occurred */
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+
+    /* Success: Let the parent terminate */
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+
+    /* On success: The child process becomes session leader */
+    if (setsid() < 0)
+        exit(EXIT_FAILURE);
+
+    /* Catch, ignore and handle signals */
+    /* TODO: Implement a working signal handler */
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+
+    /* Fork off for the second time*/
+    pid = fork();
+
+    /* An error occurred */
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+
+    /* Success: Let the parent terminate */
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+
+    /* Set new file permissions */
+    umask(0);
+
+    /* Change the working directory to the root directory */
+    /* or another appropriated directory */
+    chdir("/");
+
+    /* Close stdin, stderr, stdout file descriptors*/
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+
+    /* Close all open file descriptors */
+    int x;
+    for (x = sysconf(_SC_OPEN_MAX); x>=0; x--)
+    {
+        close (x);
+    }
+
+    /* Create the link to the syslog file */
+    openlog ("columnar", LOG_PID, LOG_DAEMON);
+}
