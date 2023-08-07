17a029a15fa8f245b21589fa95759cdbab0debea
Olivier Kessler <olivier.kessler@bioserenity.com>
2023-08-07 17:56:34 +0200

    another try to fix heapsort

--- Makefile
+++ Makefile
@@ -1,16 +1,15 @@
-columnar: main.cpp filesystem/configuration.cpp
-	g++ -o columnar.o main.cpp \
+columnar:
+	g++ -g -c main.cpp -o columnar.o main.cpp \
 		filesystem/configuration.cpp process/daemonize.cpp \
 		-W -Wall -pedantic
 
-test_heap: clean dsa/heap_test.cpp 
-	g++ -o heap_test.o \
-		dsa/heap_test.cpp \
+test_heap: clean  
+	g++ -g -c heap_test.cpp -o heap_test.o \
 		dsa/heap.cpp dsa/vector_utils.cpp -W -Wall -pedantic \
 	&& ./heap_test.o
 
-test_heap_sort: clean dsa/sort/heap_sort_test.cpp
-	g++ -o heap_sort_test.o \
+test_heap_sort: clean 
+	g++ -g -o heap_sort_test.o \
 		dsa/sort/heap_sort_test.cpp \
 		dsa/vector_utils.cpp \
 		dsa/sort/heap_sort.cpp dsa/heap.cpp -W -Wall -pedantic \
--- dsa/sort/heap_sort.cpp
+++ dsa/sort/heap_sort.cpp
@@ -1,13 +1,22 @@
 #include "../heap.h"
 #include <vector>
+#include <iostream>
+#include "../vector_utils.h"
+#include <string>
 
 void heap_sort(std::vector<int> vec) {
+    std::cout << "heap_sort()";
     Heap heap = build_max_heap(vec);
 
     for(int i=0; i < (int)vec.size(); i++) {
-        int temp = heap.data[0];
-        heap.data[0] = heap.data[vec.size() - 1];
+        std::cout << "heap_sort(for)";
+        std::cout << "\n";
+        int temp = heap.data[1];
+        std::cout << "temp = " + std::to_string(temp) + "\n";
+        heap.data[1] = heap.data[vec.size() - 1];
+        std::cout << "heap.data[vec.size() - 1] = " + std::to_string(heap.data[vec.size() - 1]) + "\n";
         heap.data[vec.size() - 1] = temp;
+        printVector(heap.data);
         max_heapify(&heap, 1);
     }
 }
